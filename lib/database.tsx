ced2b9d17dd31803c81be7a3846a6c84262d67e0
xanderg2 <XanderG2@users.noreply.github.com>
2024-04-27 15:27:46 +0100

    it works

--- lib/database.tsx
+++ lib/database.tsx
@@ -1,11 +1,31 @@
+import { readFileSync } from "fs";
+import { writeFile } from "fs/promises";
+const STORE_FILE = `${__dirname}/../store.json`;
+
+async function saveFile() {
+  await writeFile(STORE_FILE, JSON.stringify(data));
+}
+
+function loadfile() {
+  try {
+    const data = readFileSync(STORE_FILE, "utf8");
+    return JSON.parse(data);
+  } catch {
+    return Object.create(null);
+  }
+}
+
+const data = loadfile();
+
 export class Database {
   async get(key: string): Promise<any> {
-    return undefined;
+    return data[key];
   }
   async set(key: string, value: any) {
-    return;
+    data[key] = value;
+    await saveFile();
   }
   async list(prefix: string) {
-    return [];
+    return Object.keys(data).filter((key) => key.startsWith(prefix));
   }
 }
